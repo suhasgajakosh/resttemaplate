d5744f3792a0cb5cd16241172659403f63f38ea7
Gajakosh <suhas.gajakosh@capgemini.com>
2016-11-23 15:55:17 +0530

    last changes

--- src/main/java/com/rest/template/ResttemplateApplication.java
+++ src/main/java/com/rest/template/ResttemplateApplication.java
@@ -1,5 +1,6 @@
 package com.rest.template;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +9,18 @@ import com.rest.template.consume.ConsumeContactsService;
 @SpringBootApplication
 public class ResttemplateApplication {
 
-	
-	
+	private static ConsumeContactsService consumeContactService;
+
+	@Autowired
+	public ResttemplateApplication(ConsumeContactsService consumeContactService) {
+		ResttemplateApplication.consumeContactService = consumeContactService;
+	}
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(ResttemplateApplication.class, args);
-		
-		ConsumeContactsService consumeContactService = new ConsumeContactsService();
+
 		consumeContactService.consumeGetOneContact(15);
-		
+
 	}
 }
